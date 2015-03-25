c49d92df47194cb282abcd48e99f4ed78042cc75
lgh <lcliguohao@163.com>
2015-03-25 21:05:38 +0800

    Retrieve weibo text.

--- ohoauth/urls.py
+++ ohoauth/urls.py
@@ -9,6 +9,4 @@ urlpatterns = patterns(
     (r'^weibo/login/$', 'weibo.views.login'),
     (r'^weibo/success/$', 'weibo.views.success'),
     (r'^weibo/home/$', 'weibo.views.home'),
-    (r'^weibo/release/$', 'weibo.views.release'),
-    (r'^weibo/retrieve/$', 'weibo.views.retrieve'),
 )
--- weibo/views.py
+++ weibo/views.py
@@ -6,10 +6,10 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from weibo.models import User
-
+import urllib, urllib2, httplib2
 import json
-
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -39,7 +39,6 @@ def success(request):
         ('code', code),
         )
 
-    import urllib, urllib2, httplib2
     req = urllib2.Request(url, urllib.urlencode(data))
     res_data = urllib2.urlopen(req).read()
     res = json.loads(res_data)
@@ -55,15 +54,9 @@ def success(request):
         user.access_token = res['access_token']
         user.expires_in = res['expires_in']
 
-        url = 'https://api.weibo.com/2/users/show.json'
-        paras = urllib.urlencode({
-            'uid' : user.uid,
-            'access_token' : user.access_token,
-        })
-        http = httplib2.Http()
-        response, content = http.request(url + '?' + paras)
-        info = json.loads(content)
+        info = user_info(user.uid, user.access_token)
         user.name = info['screen_name']
+
         user.save()
         request.session['uid'] = res['uid']
         return HttpResponseRedirect('/weibo/home/')
@@ -74,49 +67,78 @@ def home(request):
         return HttpResponseRedirect('/weibo/login/')
     user = User.objects.filter(uid = request.session['uid']).first()
 
+    template_data = {}
+    template_data['user'] = user
+    template_data['action'] = 'none'
+    template_data['status'] = 'none'
+
+    if 'action' in request.GET:
+        if request.GET['action'] == 'release':
+            template_data['action'] = 'release'
+            if (request.method == 'POST'):
+                content = request.POST['content']
+                if not content:
+                    template_data['status'] = u'微博内容不能为空'
+                    return render_to_response(
+                        'home_weibo.html',template_data,
+                        context_instance = RequestContext(request)
+                    )
+                response_json = release_weibo(content, user.access_token)
+                if 'error' in response_json:
+                    template_data['status'] = response_json['error']
+                    return render_to_response(
+                        'home_weibo.html',template_data,
+                        context_instance = RequestContext(request)
+                    )
+                template_data['status'] = u'微博成功发布'
+            return render_to_response(
+                'home_weibo.html',template_data,
+                context_instance = RequestContext(request)
+            )
+        elif request.GET['action'] == 'retrieve':
+            template_data['action'] = 'retrieve'
+            response_json = retrieve_weibo(user.uid, user.access_token)
+            template_data['weibo_text_list'] = []
+            for item in response_json['statuses']:
+                template_data['weibo_text_list'].append(item['text'])
+            return render_to_response(
+                'home_weibo.html',template_data,
+                context_instance = RequestContext(request)
+            )
     return render_to_response(
-        'home_weibo.html', {
-            'user' : user,
-            'status' : 'Welcome',
-        })
+        'home_weibo.html',template_data,
+        context_instance = RequestContext(request)
+    )
 
 
-def release(request):
-    if 'uid' not in request.session:
-        return HttpResponseRedirect('/weibo/login/')
-    user = User.objects.filter(uid = request.session['uid']).first()
-
-    if (request.method == 'POST'):
-        import urllib, urllib2
-        url = 'https://api.weibo.com/2/statuses/update.json'
-        content = request.POST['content']
-        print content
-        data = (
-            ('status', content),
-            ('access_token', user.access_token),
-        )
-        req = urllib2.Request(url, urllib.urlencode(data))
-        res_data = urllib2.urlopen(req).read()
-        res = json.loads(res_data)
-        if 'error' in res:
-            return HttpResponse(res_data)
-        return render_to_response(
-            'release_weibo.html', {
-                'user' : user,
-                'status' : u'微博成功发布',
-            })
-    else:
-        return render_to_response(
-            'release_weibo.html', {
-                'user' : user,
-                },
-            context_instance = RequestContext(request)
-        )
+def user_info(uid, access_token):
+    url = 'https://api.weibo.com/2/users/show.json'
+    paras = urllib.urlencode({
+        'uid' : uid,
+        'access_token' : access_token,
+    })
+    http = httplib2.Http()
+    response, content = http.request(url + '?' + paras)
+    return json.loads(content)
 
 
-def retrieve(request):
-    if 'uid' not in request.session:
-        return HttpResponseRedirect('/weibo/login/')
-    user = User.objects.filter(uid = request.session['uid']).first()
+def release_weibo(content, access_token):
+    url = 'https://api.weibo.com/2/statuses/update.json'
+    data = (
+        ('status', content),
+        ('access_token', access_token),
+    )
+    req = urllib2.Request(url, urllib.urlencode(data))
+    res_data = urllib2.urlopen(req).read()
+    return json.loads(res_data)
 
-    return HttpResponseRedirect('/weibo/home/?action=retrieve')
+
+def retrieve_weibo(uid, access_token):
+    url = 'https://api.weibo.com/2/statuses/user_timeline.json'
+    paras = urllib.urlencode({
+        'uid' : uid,
+        'access_token' : access_token,
+    })
+    http = httplib2.Http()
+    response, content = http.request(url + '?' + paras)
+    return json.loads(content)
